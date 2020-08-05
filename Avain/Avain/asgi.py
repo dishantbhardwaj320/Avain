ff8e14ef792f008da8231ec6913acf49171eb41c
vishavbangotra <44341523+vishavbangotra@users.noreply.github.com>
2020-08-06 02:28:34 +0530

    created django3

--- Avain/Avain/asgi.py
+++ Avain/Avain/asgi.py
@@ -1,16 +1,16 @@
 """
-ASGI config for avain project.
+ASGI config for Avain project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'avain.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Avain.settings')
 
 application = get_asgi_application()
